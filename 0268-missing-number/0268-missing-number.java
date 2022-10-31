cd71978bbb69ea58134b9bd130dbddaaf33edfa8
himarekhap91 <116838628+himarekhap91@users.noreply.github.com>
2022-10-31 06:52:11 -0400

    Time: 0 ms (100.00%), Space: 43.1 MB (91.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,10 @@
-class Solution {
-    public int missingNumber(int[] nums) {
      // Approach 2 - We know the expected numbers that needs to be present as the range is 0 to n 
      // We calculate the expectedSum as we know the range 
     // We then calculate the currentSum with the elements present
     // Finally we subtract the sum 
+class Solution {
+    public int missingNumber(int[] nums) {
+
      int range = nums.length;
      int expectedSum = 0;
      int currentSum = 0;
