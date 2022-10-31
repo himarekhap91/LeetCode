04f15098b9eea218c9dadb42aca6ef7384f92ae9
himarekhap91 <116838628+himarekhap91@users.noreply.github.com>
2022-10-31 06:38:38 -0400

    Time: 8 ms (24.28%), Space: 51.2 MB (43.04%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // Approach 1 - Sort the array. Loop from 0 to n and check if the number is equal to the value in that position 
+        Arrays.sort(nums);
+        int length = nums.length + 1;
+        for(int i = 0 ; i < length; i++){
+         if(i == nums.length){
+                return i;
+            } else if(!(i == nums[i])){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
